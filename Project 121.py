9b69fd73ad2165d80720dd62c198f7ca16771322
Zachary-Skinner <112426539+Zachary-Skinner@users.noreply.github.com>
2022-12-01 00:02:17 -0600

    Update Project 121.py
    added find highest in a column function

--- Project 121.py
+++ Project 121.py
@@ -13,13 +13,60 @@ file
 
 # In[24]:
 
-
+dict2 = {1:"A", 2:"B", 3: "C", 4:"D", 5 : "E", 6 : "F", 7 : "G"}
 #Created a dictionary to store all the states
 dict_states={}
+dict_statenum={}
 for i in range(23):       
     dict_states[file.iloc[i,0]]= i
+    dict_statenum[i]=file.iloc[i,0]
 
-    
+#find the highest value in a colum and tells you what row that highest number belongs to
+def Hi(x):
+    b=0
+    CurrentRow=0
+    Row=0
+    Highest=0
+    #iterates through all values in colum 
+    for i in ws.iter_rows(min_row = 2,
+                         max_row = 24,
+                         min_col=x,
+                         max_col=x,
+                         values_only=True):
+        
+        #makes string of the value in cell by default iter_rows outputs cell calue fo 12.6 as (12.6,)
+        L=str(i)
+        #num is a string that will be used to store the cell value with out the added () and , 
+        num=""
+        #strips cell value of everything other then the number
+        for n in L:
+            try: 
+                #crow tracks which row the cell we are looking at is in
+                crow=int(n)
+                num=num+n
+                #print(thing)
+
+            except ValueError:
+                if n == ".":
+                    num=num+n
+                    #print(thing)
+                pass
+        if num == "":
+            num="0"
+        #coverts num into float
+        num=float(num)
+        #checks if new number is higher
+        if Highest < num:
+            Highest=num
+            Row=CurrentRow
+        CurrentRow+=1
+    state=dict_statenum[Row]
+    category=ws[str(dict2[x])+"1"].value
+    print("The highest number in the category: ",category," is:",Highest," which belongs to: ",state)
+
+for i in range(6):
+    Hi(i+2)
+    #Lo(i+2)    
 class state():
     #User gets to pick a state 
     def __init__(self, name):
